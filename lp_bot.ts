5b8d59f42a1cf7d7c517ea479655a1651ce52e7e
gegego <wejaq@pc-weng.>
2026-04-06 12:46:40 +0200

    update

--- lp_bot.ts
+++ lp_bot.ts
@@ -17,6 +17,7 @@ async function randomSleep(min: number, max: number) {
 
 
 const config_lp = loadConfig('./config.json')
+console.log(config_lp)
 const connection = new Connection(config_lp["solana_url"], {commitment: "confirmed",
                                     confirmTransactionInitialTimeout: 60000, // 增加到60秒
                                 });                                
@@ -40,24 +41,7 @@ class PoolManager {
 (async () => {   
     const wallet = loadWallet('./wallet.json'); // 替换为你的私钥文件路径
 
-    const pool_config_list = [
-        {
-            'pool_addr':"HbjYfcWZBjCBYTJpZkLGxqArVmZVu3mQcRudb6Wg1sVh",
-            'action': "sell",
-            'binstep': 20,
-            'upper_bin':35,
-            'stopbin':10,
-            'rebuild':5
-        },
-        {
-            'pool_addr':"53RSBX3tsax8KLnEhm8ahScK1khySNPhHFSTPoZpZq2J",
-            'action': "sell",
-            'binstep': 2,
-            'upper_bin':60,
-            'stopbin':30,
-            'rebuild':10
-        }        
-    ]
+    const pool_config_list = config_lp["pool_list"]
     while(true){
         for(const pool_conf of pool_config_list){
             try{
@@ -121,56 +105,13 @@ class PoolManager {
         }
         await randomSleep(50, 70)        
     }
-
-    // const { userPositions } = await dlmmPool.getPositionsByUserAndLbPair(wallet.publicKey);
-    // for(const position of userPositions){
-    //     const minBinId = position.positionData.lowerBinId;
-    //     const maxBinId = position.positionData.upperBinId;            
-    //     if((maxBinId - minBinId) < 20){
-    //         await removeLiquidity_single(dlmmPool, wallet, connection, position);  
-    //     }   
-    // }
-
-    // const currentPrice = dlmmPool.fromPricePerLamport(Number(activebin.price));
-    // const currentbin = getBinFromPrice(currentPrice, dlmmPool.lbPair.binStep, 9, 6);
-    // const currentbin2 = dlmmPool.getBinIdFromPrice(activebin.price, false);
-    
-    // const maxBinId = position.positionData.upperBinId;   
-    // await removeLiquidity_trade(dlmmPool, wallet, connection);
-    // const binid = Math.floor(minBinId + (activebin.binId - minBinId)*0.6);
-    // console.log(binid)
-    // await dlmmPool.refetchStates();
-    // const activebin = await dlmmPool.getActiveBin();
-    // const minBinId_add = activebin.binId-60
-    // const maxBinId_add = activebin.binId-1
-    // await createPosition_sol(dlmmPool, wallet, connection, minBinId_add, maxBinId_add, Math.floor(4*1e9));
-
-    // await dlmmPool.refetchStates();
-    // const activebin = await dlmmPool.getActiveBin();
-    // const maxBinId_add = activebin.binId+5
-    // const minBinId_add = activebin.binId
-    // await createPosition_token(dlmmPool, wallet, connection, minBinId_add, maxBinId_add, 1);
-
-    // await addLiquidity_part(dlmmPool, wallet, connection, minBinId_add, maxBinId_add, Math.floor(0.3*1e9));
-    // checkRebalanceConditions(dlmmPool, wallet, minBinId, maxBinId)
-    // rebalance(wallet,connection, dlmmPool, minBinId, maxBinId);
-    // await totally_newposition(wallet, connection, dlmmPool, 2.3, minBinId, maxBinId);
-    //await removeLiquidity_part(dlmmPool, wallet, connection, minBinId, maxBinId)
 })();
 
-    // const POOL_ADDRESS = new PublicKey(pool_addr);
-    // const dlmmPool = await DLMM.create(connection, POOL_ADDRESS);   
-    // console.log("--- 深度结构探测开始 ---");pool_addr
-    // console.log(util.inspect(userPositions[0], { 
-    //     showHidden: false, 
-    //     depth: 5,         // 递归深度，5层通常足够了
-    //     colors: true, 
-    //     compact: false    // 格式化输出，不压缩在一行
-    // }));
-    // console.log("--- 深度结构探测结束 ---");
-    // const result = await dlmmPool.getBinsBetweenMinAndMaxPrice(0.0000231, 0.000026)
-    // const minBinId = dlmmPool.getBinIdFromPrice(Number(dlmmPool.toPricePerLamport(0.0000241)),false);
-    // const maxBinId = dlmmPool.getBinIdFromPrice(Number(dlmmPool.toPricePerLamport(0.0000245)), true);
-    // console.log("minbinid:",minBinId);
-    // console.log("maxbinid:",maxBinId);
-    // console.log(currentbin2)
+// console.log("--- 深度结构探测开始 ---");pool_addr
+// console.log(util.inspect(userPositions[0], { 
+//     showHidden: false, 
+//     depth: 5,         // 递归深度，5层通常足够了
+//     colors: true, 
+//     compact: false    // 格式化输出，不压缩在一行
+// }));
+// console.log("--- 深度结构探测结束 ---");
